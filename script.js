01d1540cc8e9afe0fb93b864f4f5e37ac5de6490
John Comstock <johncomstock@Johns-MacBook-Pro-2.local>
2022-03-18 16:03:39 -0400

    almost done

--- script.js
+++ script.js
@@ -470,7 +470,7 @@ const overallBalance2 = accounts
 
 console.log(overallBalance2);
 
-*/
+
 
 // sorting
 //sort method mutates og array
@@ -483,3 +483,124 @@ movements.sort((a, b) => a - b);
 console.log(movements);
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+
+
+// we are used to handwriting arrays
+
+//empty array and full array fill method
+const x = new Array(7);
+const ranArr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+x.fill(1, 3, 5);
+
+ranArr.fill(23, 4, 6);
+console.log(x);
+console.log(ranArr);
+
+// array.from
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const randomArr = Array.from(
+  { length: 100 },
+  cur => (cur = Math.trunc(Math.random() * 6) + 1)
+);
+console.log(randomArr);
+
+const movementsUI = Array.from(document.querySelectorAll('.movements__value'));
+console.log(movementsUI);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent)
+  );
+  console.log(movementsUI);
+});
+
+
+
+// exercise 1
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((accum, cur) => accum + cur, 0);
+
+console.log(bankDepositSum);
+
+// exercise 2
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((accum, cur) => (cur >= 1000 ? ++accum : accum), 0);
+console.log(numDeposits1000);
+
+let a = 10;
+
+// a++ returns value prior to increment
+console.log(a++);
+
+// exercise 3 object that contains deposits and withdrawals
+
+const sums = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(sums);
+
+// exercise 4
+// this is a nice title -> This Is a Nice Title
+
+const convertTitleCase = function (title) {
+  const exceptions = ['a', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+
+  return titleCase;
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => {
+  dog.recommendedFood = dog.weight ** 0.75 * 28;
+});
+
+console.log(dogs);
+
+const findOwnerDog = function (owner, dogArr) {
+  const sarahDog = dogArr.find(dog => dog.owners.includes('Sarah'));
+  console.log(sarahDog);
+
+  const eatTooMuch =
+    sarahDog.curFood >= sarahDog.recommendedFood ||
+    sarahDog.curFood <= sarahDog.recommendedFood;
+};
+
+findOwnerDog('Sarah', dogs);
